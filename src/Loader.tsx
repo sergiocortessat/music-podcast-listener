8d8d485fdd5609b1ca80edc8f9140187f70b2208
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2023-09-04 14:49:46 +0200

    Add vercel json

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center items-center relative">
       <span className="circle animate-loader"></span>
       <span className="circle animate-loader animation-delay-200"></span>
       <span className="circle animate-loader animation-delay-400"></span>
