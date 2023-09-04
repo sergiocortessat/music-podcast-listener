7ac366a327f86f74b67cd058664bd825177daf20
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2023-09-04 14:34:20 +0200

    Add changes

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,20 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
-// import { Podcast } from './../store/types';
-// import { getTopPodcasts  } from '../services/api';
 import { useGetPodcastData } from "../hooks/useGetPodcastData";
 
 const MainView: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const { podcastTop, error, isLoading } = useGetPodcastData();
+  const [filteredPodcasts, setFilteredPodcasts] = useState(podcastTop?.feed.entry || []);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
-
-  // Aquí necesitas cargar tus podcasts (posiblemente desde Redux)
-  // const podcasts: any[] = []; // Por ahora, solo un array vacío.
-  // const [podcast, setPodcast] = useState<any>([]);
-
   // // const filteredPodcasts = podcasts.filter(podcast =>
   // //     podcast.title.includes(searchTerm) || podcast.author.includes(searchTerm)
   // // );
@@ -30,18 +23,12 @@ const MainView: React.FC = () => {
       />
 <div className="flex flex-wrap items-center justify-center gap-5">
   {podcastTop &&
-    podcastTop.feed.entry.map((podcast: any) => {
+    podcastTop.feed.entry.map((podcast) => {
       const podcastId = podcast.id.attributes["im:id"];
       const podcastUrl = `/podcast/${podcastId}`;
       return (
         <div key={podcast.id.attributes["im:id"]} className="w-96 h-96">
           <a href={podcastUrl}>
-            {/* <img src={podcast['im:image'][2].label} alt={podcast['im:name'].label} />
-                        <h1>{podcast['im:name'].label}</h1>
-                        <h2>Author: {podcast['im:artist'].label}</h2> */}
-            {/* <h3>{podcast.category.attributes.label}</h3> */}
-            {/* <h4>{podcast['im:releaseDate'].attributes.label}</h4> */}
-            {/* add author */}
             <div className="card w-full glass h-96">
               <figure>
                 <img
--- src/hooks/useGetPodcastDetails.tsx
+++ src/hooks/useGetPodcastDetails.tsx
@@ -2,22 +2,18 @@ import { useParams } from 'react-router-dom';
 import {getPodcastDetails} from '../services/api';
 import useSWR from 'swr';
 import { getFromLocalStorage, saveToLocalStorage } from '../helpers/storage';
+import {UsePodcastDetailsHook} from '../types/podcastDetail'
 
-const useGetPodcastDetails = () => {
-    // I need to get the id from the url
+const useGetPodcastDetails = (): UsePodcastDetailsHook => {
     const podcastId = useParams().podcastId;
+    const lastFetched = getFromLocalStorage('lastFetched');
     const initialData = getFromLocalStorage(`podcastDetails-${podcastId}`);
-    const shouldFetch = !initialData;
+    const shouldFetch = !initialData || (new Date().getTime() - new Date(lastFetched).getTime()) > 86400000;
     const fetcher = shouldFetch ? getPodcastDetails : null;
     const { data: podcastInfo, error, isLoading } = useSWR(podcastId, fetcher, { fallbackData: initialData })
     if (podcastInfo && shouldFetch) {
         saveToLocalStorage(`podcastDetails-${podcastId}`, podcastInfo);
     }
-    // const { data: podcastDetails, error, isLoading } = useSWR('https://itunes.apple.com/us/podcast/id1474561710/episodes', getPodcastTrackFeed)
-    // console.log('podcastDetails', podcastDetails);
-    // console.log('error', error);
-    // console.log('isLoading', isLoading);
-    // console.log('episodeId', podcastId);
     console.log('episodeDetails', initialData);
     return ({podcastInfo, error, isLoading, podcastId})
 }
